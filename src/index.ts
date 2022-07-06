a4df0f1955a4a5aaee65e0ac004e67dc965eed7d
Vladimir Minakov <minakov@gmail.com>
2022-07-06 15:05:53 +0000

    Add google client

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,24 @@
 import { initializeApp, getApps } from 'firebase-admin/app';
 import { getStorage } from 'firebase-admin/storage';
 import * as functions from "firebase-functions";
+import { GoogleAuth } from 'google-auth-library';
 
 if (!getApps().length) {
-  initializeApp()
+    initializeApp()
 }
 
-export const deploy = functions.https.onRequest((request, response) => {
-  functions.logger.info("deploy", { structuredData: true });
-  const bucket = getStorage().bucket();
-  response.send(bucket.name);
+const auth = new GoogleAuth({
+    scopes: 'https://www.googleapis.com/auth/cloud-platform'
+});
+
+export const deploy = functions.https.onRequest(async (request, response) => {
+    const client = await auth.getClient();
+    const res = await client.request({
+        url: `https://firebasehosting.googleapis.com/v1beta1/sites/${process.env.SITE_ID}/versions`
+    });
+
+    const bucket = getStorage().bucket();
+    functions.logger.info("deploy", bucket.name);
+
+    response.send(await res.data);
 });
